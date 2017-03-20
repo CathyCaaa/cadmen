bd2c875bb6be18d44c824edc90e95c49fc7440ba
CathyCaaa <yiu0227@hotmail.com>
2017-03-20 17:44:49 +0800

    Create plot_functions.R

---
+++ plot_functions.R
@@ -0,0 +1,19 @@
+plot_chart <- function(plot_data, plot_type, var_name)
+{
+  # Plot the chart
+  pl <- ggplot(plot_data, aes(x, data_points)) + geom_point(colour = "navyblue", size = 6)
+  pl <- pl + 
+    geom_line(aes(x, data_points, colour = "Data points trace"), data = plot_data) +
+    geom_line(aes(x, center, colour = "Center"), data = plot_data, lwd = 2) +
+    geom_line(aes(x, lcl, colour = "LCL"), data = plot_data, lwd = 2) +
+    geom_line(aes(x, ucl, colour = "UCL"), data = plot_data, lwd = 2) +
+    
+    # Add legend
+    scale_colour_manual(values = c("yellow", "blue", "red", "red")) +
+    # Add labels and title
+    labs(title = paste("Control chart displayed:", plot_type) ,x = "Group", y = var_name)
+  
+  return(pl)
+
+  
+}
